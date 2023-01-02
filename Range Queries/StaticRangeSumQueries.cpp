114779e70e2057a67e68f65c6b8a65191924ff51
Aakarsh Jain <jainaakarsh09@gmail.com>
2023-01-02 05:42:26 +0530

    Added solution for Static Range Sum Queries

---
+++ Range Queries/StaticRangeSumQueries.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define int long long int
+
+const int mod = 1000000007;
+const int inf = LLONG_MAX;
+const int neg_inf = LLONG_MIN;
+
+#define nline               "\n"
+#define rep(xx)             for(int aa = 0; aa < xx; aa++)
+#define rep_u(ii, aa, bb)   for(int ii = aa; ii < bb; ii++)
+#define rep_d(ii, aa, bb)   for(int ii = aa; ii >= bb; ii--)
+#define sz(x)               (int)x.size()
+#define pb                  push_back
+#define mp                  make_pair
+#define all(x)              (x).begin(), (x).end()
+#define sum_all(x)          accumulate(all(x), 0)
+#define max_idx(x)          max_element(all(x)) - x.begin()
+#define min_idx(x)          min_element(all(x)) - x.begin()
+#define pcnt(x)             __builtin_popcountll(x)
+#define clz(x)              __builtin_clzll(x)
+#define ctz(x)              __builtin_ctzll(x)  
+#define prec(x)             setprecision(x) << fixed
+
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    #ifdef BlackPanther112358
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+        freopen("error.txt", "w", stderr);
+    #endif
+
+    // Taking input from console
+    int n, q;
+    cin >> n >> q;
+    vector<int> x(n);
+    rep_u(i, 0, n) cin >> x[i];
+
+    // Creating a prefix sum array
+    vector<int> pref(n + 1, 0);
+    rep_u(i, 1, n + 1) pref[i] = pref[i - 1] + x[i - 1];
+
+    // Processing queries
+    rep(q){
+        int a, b;
+        cin >> a >> b;
+        cout << pref[b] - pref[a - 1] << nline;
+    }
+
+    return 0;
+
+}
