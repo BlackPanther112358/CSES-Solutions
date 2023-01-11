d9623aeb45e9050e23566ecc7dcce836c4cea3ba
Aakarsh Jain <jainaakarsh09@gmail.com>
2023-01-12 03:51:15 +0530

    Added solution for Josephus Problem I

---
+++ Sorting and Searching/JosephusProblemI.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+#define int long long int
+
+const int mod = 1000000007;
+const int inf = LLONG_MAX;
+const int neg_inf = LLONG_MIN;
+
+#define nline               "\n"
+#define rep(xx)             for(int aa = 0; aa < xx; aa++)
+#define rep_u(ii, aa, bb)   for(int ii = aa; ii < bb; ii++)
+#define rep_d(ii, aa, bb)   for(int ii = aa; ii >= bb; ii--)
+#define sz(x)               (int)x.size()
+#define pb                  push_back
+#define mp                  make_pair
+#define all(x)              (x).begin(), (x).end()
+#define sum_all(x)          accumulate(all(x), 0)
+#define max_idx(x)          max_element(all(x)) - x.begin()
+#define min_idx(x)          min_element(all(x)) - x.begin()
+#define pcnt(x)             __builtin_popcountll(x)
+#define clz(x)              __builtin_clzll(x)
+#define ctz(x)              __builtin_ctzll(x)  
+#define prec(x)             setprecision(x) << fixed
+
+int32_t main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    #ifdef BlackPanther112358
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+        freopen("error.txt", "w", stderr);
+    #endif
+
+    // Take input from console
+    int n, k;
+    cin >> n;
+    // Set k = 1 as we have to skip a single child on each turn
+    k = 1;
+
+    // Create a ordered_set to store the children
+    tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> arr;
+    // Populating the ordered_set to represent the children
+    rep_u(i, 0, n) arr.insert(i + 1);
+
+    // Iterate until there is no child left
+    int curr = 0;
+    while(sz(arr)){
+        // Skip k children
+        curr += k;
+        curr %= sz(arr);
+        // Remove the kth child
+        int temp = *arr.find_by_order(curr);
+        arr.erase(temp);        
+        cout << temp << " ";
+    }
+    cout << nline;
+
+    return 0;
+
+}
