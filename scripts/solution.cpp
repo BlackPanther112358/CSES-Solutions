701aee8058eb1fe7a0ad52352304e148daa4f3c0
Aakarsh Jain <jainaakarsh09@gmail.com>
2023-01-22 13:05:50 +0530

    Added solution

--- scripts/solution.cpp
+++ scripts/solution.cpp
@@ -1,3 +1,4 @@
+// Increasing Subsequence
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
@@ -44,29 +45,29 @@ int32_t main(){
 	vector<int> a(n);
 	rep_u(i, 0, n) cin >> a[i];
 
-	// dp[i][j] = Best score of both players when array is a[i...j]
-	// Iterate using Top up DP approach on lenght of array
-	// Can optimize space complexity to O(n) as we only store dp[i][j] and dp[i][j+1] at any time
-	vector<pair<int, int>> dp;
-	// Base case: dp[i][i] = a[i] for first player and 0 for second player
-	rep_u(i, 0, n) dp.pb(mp(a[i], 0));
+	// dp[i] stores the smallest value of the last element of an increasing subsequence of length i+1
+	// Also, as a smaller length subsequence will always be a subsequence of a larger length subsequence, the value of dp[i] will always be smaller than dp[i+1]
+	// Thus dp[i] is always increasing. This allows us to use binary search the maximum length of an increasing subsequence ending at a[i]
+	vector<int> dp(n + 1, inf);
+	rep_u(i, 0, n){
+		// We need to find the smallest j, such that dp[j] >= a[i]
+		// Thus, dp[j] (last element of an increasing subsequence of length j+1) is a[i]
+		// This can be done using binary search
+		int idx = lower_bound(all(dp), a[i]) - dp.begin();
+		dp[idx] = a[i];
+	}
 
-	// Iterate over length of array
-	rep_u(i, 2, n + 1){
-		// Store the previous dp array in temp
-		vector<pair<int, int>> temp = dp;
-		dp.clear();
-		// Iterate using sliding window of length i
-		rep_u(j, 0, n - i + 1){
-			// Player 1 can choose a[j] or a[j + i - 1] and then best scores of both players are stored in dp[j + 1] and dp[j] respectively 
-			dp.pb(max(mp(a[j] + temp[j + 1].second, temp[j + 1].first), mp(a[j + i - 1] + temp[j].second, temp[j].first)));
+	// Find the maximum length of an increasing subsequence
+	// We also added an extra element to dp in case the entire array is an increasing subsequence
+	rep_u(i, 0, n + 1){
+		// If dp[i] is inf, then no increasing subsequence of length i+1 exists
+		if(dp[i] == inf){
+			cout << i << nline;
+			break;
 		}
 	}
 
-	// Answer is net score of game when array is a[0...n-1]
-	cout << dp[0].first;
-
-	// TIME COMPLEXITY: O(n^2)
+	// TIME COMPLEXITY: O(n*log(n))
 	// SPACE COMPLEXITY: O(n)
 
     return 0;
