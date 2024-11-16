ffc5ac0c66069d719e9eca57cb6e479cd23751dd
Aakarsh Jain <jainaakarsh09@gmail.com>
2024-11-17 03:15:36 +0530

    Updating

--- scripts/solution.cpp
+++ scripts/solution.cpp
@@ -1,3 +1,4 @@
+//  Dynamic Range Sum Queries
 #include <bits/stdc++.h>
 #include <ext/pb_ds/assoc_container.hpp>
 #include <ext/pb_ds/tree_policy.hpp>
@@ -9,6 +10,36 @@ using namespace __gnu_pbds;
 const int mod = 1000000007;
 const int inf = LLONG_MAX;
 const int neg_inf = LLONG_MIN;
+
+class fenwicked_tree{
+private:
+    int size;
+    vector<int> arr;
+
+public:
+    fenwicked_tree(int n){
+        size = n;
+        arr.resize(n, 0);
+    }
+
+    void update(int idx, int val){
+
+    }
+
+    int prefix_sum(int idx){
+        int ans = 0;
+        while(idx >= 0){
+            ans += arr[idx];
+            idx = idx&(idx + 1) - 1;
+        }
+        return ans;
+    }
+
+    int range_sum(int l, int r){
+        if(l == 0) return prefix_sum(r);
+        return prefix_sum(r) - prefix_sum(l - 1);
+    }
+};
 
 int32_t main(){
 
@@ -22,44 +53,7 @@ int32_t main(){
         freopen("error.txt", "w", stderr);
     #endif
 
-    // Taking input from console
-    int n, q;
-    cin >> n >> q;
-
-    // We will use binary jumping to solve this problem
-    // Initialize the jump array, where jump[i][j] = 2^jth parent of i
-    // Since we have to find 10^9 the parent in the worst case, we will need log2(10^9) = 30 jumps
-    int jump[n][30];
-
-    // Take input of the parent of each node
-    for(int i = 0; i < n; i++){
-        cin >> jump[i][0];
-        jump[i][0]--;
-    }
-
-    // Construct the jump array
-    // We iterate over the size of jump so that for larger values of j, we can use the values of smaller j
-    for(int j = 1; j < 30; j++){
-        for(int i = 0; i < n; i++){
-            // 2^jth parent of i is 2^(j-1)th parent of 2^(j-1)th parent of i
-            jump[i][j] = jump[jump[i][j-1]][j-1];
-        }
-    }
-
-    // Process the queries
-    while(q--){
-        int x, k;
-        cin >> x >> k;
-        x--;
-        // We iterate over the bits of k from the most significant bit to the least significant bit
-        for(int i = 29; i >= 0; i--){
-            // If the ith bit is set, we jump to the 2^ith parent of x
-            if(k & (1 << i)){
-                x = jump[x][i];
-            }
-        }
-        cout << x + 1 << '\n';
-    }
+    
 
 	// TIME COMPLEXITY: O(n*log(k) + q*log(k))
 	// SPACE COMPLEXITY: O(n*log(k))
